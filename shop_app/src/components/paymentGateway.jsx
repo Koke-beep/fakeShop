70086c410c95fc825392345d244e18a01e1641f3
Alvaro V.D <alvarovalladia@gmail.com>
2022-11-23 22:50:10 +0100

    router works

---
+++ shop_app/src/components/paymentGateway.jsx
@@ -0,0 +1,5 @@
+export default function PaymentGateway() {
+    return(
+        <h1 style={{fontSize: '4rem'}}> PAYMENT GATEWAY WORKS. SRRY</h1>
+    )
+}
--- shop_app/src/components/shared/cart.link.tsx
+++ shop_app/src/components/shared/cart.link.tsx
@@ -1,12 +1,14 @@
 import { ReactElement} from 'react'
 import '../../styles/cartLink.scss'
 import carrito  from '../../assets/svg/carrito.svg'
-
+import {Link} from "react-router-dom";
 export default function CartLinkComponent({cartItems}: {[key: string]: number}): ReactElement<Element>{
     return (
         <div className='cart'>
+            <Link to="/paymentGateway">
             <img className='cart__image' src={carrito}></img>
             <span className='cart__count'>{cartItems}</span>
+            </Link>
         </div>
     )
 }
